07fe517f2b95baae5e1a08491c4389c8808afd69
Godless-PC <skif926@mail.ru>
2023-01-05 07:35:29 +0300

    Comments

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import pygame
 from copy import deepcopy
 from random import choice, randrange
 
+# Количество плиток
 Width = 10
 Height = 20
+# размер плитки
 Title = 45
+# разрешение поля игры
 Game_res = Width * Title, Height * Title
+# разрешение общее
 RES = 750, 940
+# количество кадров
 FPS = 60
+# переменная остановки игры
 pause = 1
 state = True
 pygame.init()
@@ -15,9 +21,10 @@ pygame.display.set_caption('Tetris')
 sc = pygame.display.set_mode(RES)
 game_sc = pygame.Surface(Game_res)
 clock = pygame.time.Clock()
-
+# сетка поля
 grid = [pygame.Rect(x * Title, y * Title, Title, Title) for x in range(Width) for y in range(Height)]
 
+# кординаты фигур
 figurespos = [[(-1, -1), (-2, -1), (0, -1), (1, -1)],
               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
@@ -27,32 +34,38 @@ figurespos = [[(-1, -1), (-2, -1), (0, -1), (1, -1)],
               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
 figures = [[pygame.Rect(x + Width // 2, y + 1, 1, 1) for x, y in figpos] for figpos in figurespos]
 figure_rect = pygame.Rect(0, 0, Title - 2, Title - 2)
+# карта игрового поля
 field = [[0 for _ in range(Width)] for _ in range(Height)]
+# падение фигуры
 anim_count = 0
 anim_speed = 60
 anim_limit = 2000
+# картинки
 bg = pygame.image.load('img/1.jpg').convert()
 game_bg = pygame.image.load('img/2.jpg').convert()
 pause_bg = pygame.image.load('img/3.jpg').convert()
+# шрифты
 main_font = pygame.font.Font('font/Vasek.ttf', 100)
 font = pygame.font.Font('font/Vasek.ttf', 65)
 score = 0
 lines = 0
+# получаемые очки
 scores = {0: 0, 1: 100, 2: 300, 3: 700, 4: 1500}
+# рандомный цвет
 get_color = lambda: (randrange(30, 256), randrange(30, 256), randrange(30, 256))
 
 title_tetris = main_font.render('Tetris', True, pygame.Color('Pink'))
 title_score = font.render('score:', True, pygame.Color('Green'))
 title_record = font.render('record:', True, pygame.Color('Yellow'))
 
-
-
+# создаётся фигура и выбирается следующая
 figure = deepcopy(choice(figures))
 next_figure = deepcopy(choice(figures))
 color = get_color()
 next_color = get_color()
 
 
+# проверка столкновения
 def check_borders():
     if figure[i].x < 0 or figure[i].x > Width - 1:
         return False
@@ -61,6 +74,7 @@ def check_borders():
     return True
 
 
+# получение рекорда
 def get_record():
     try:
         with open('record') as f:
@@ -70,6 +84,7 @@ def get_record():
             f.write('0')
 
 
+# запись рекорда
 def set_record(lrecord, lscore):
     rec = max(int(lrecord), lscore)
     with open('record', 'w') as f:
@@ -104,6 +119,7 @@ while True:
         figure_old = deepcopy(figure)
         for i in range(4):
             figure[i].x += dx
+            # если фигура за границами восстанавливаем
             if not check_borders():
                 figure = deepcopy(figure_old)
                 break
@@ -115,6 +131,7 @@ while True:
             for i in range(4):
                 figure[i].y += 1
                 if not check_borders():
+                    # запоминание фигуры создание новой
                     for i in range(4):
                         field[figure_old[i].y][figure_old[i].x] = color
                     figure = next_figure
@@ -123,11 +140,12 @@ while True:
                     next_color = get_color()
                     anim_limit = 2000
                     break
-
+        # вращение фигур
         centr_rotate = figure[0]
         figure_old = deepcopy(figure)
         if rotate:
             for i in range(4):
+                # вращение на 90 по часовой
                 x = figure[i].y - centr_rotate.y
                 y = figure[i].x - centr_rotate.x
                 figure[i].x = centr_rotate.x - x
@@ -135,7 +153,7 @@ while True:
                 if not check_borders():
                     figure = deepcopy(figure_old)
                     break
-
+        # удаление заполненых линий
         lastLine = Height - 1
         lines = 0
         for row in range(Height - 1, -1, -1):
@@ -147,10 +165,12 @@ while True:
             if count < Width:
                 lastLine -= 1
             else:
+                # увеличиваем скорость падения фигуры
                 anim_speed += 3
                 lines += 1
-
+        # очки за линии
         score += scores[lines]
+        # отрисовка поля сетки
         [pygame.draw.rect(game_sc, (40, 40, 40), irect, 1) for irect in grid]
 
         for i in range(4):
@@ -158,13 +178,14 @@ while True:
             figure_rect.y = figure[i].y * Title
 
             pygame.draw.rect(game_sc, color, figure_rect)
-
+        # отрисовка поля
         for y, raw in enumerate(field):
             for x, col in enumerate(raw):
                 if col:
                     figure_rect.x = x * Title
                     figure_rect.y = y * Title
                     pygame.draw.rect(game_sc, col, figure_rect)
+        # отрисовка следующей фигуры
         for i in range(4):
             figure_rect.x = next_figure[i].x * Title + 380
             figure_rect.y = next_figure[i].y * Title + 185
@@ -174,15 +195,17 @@ while True:
         sc.blit(font.render(str(score), True, pygame.Color('white')), (550, 840))
         sc.blit(title_record, (535, 680))
         sc.blit(font.render(record, True, pygame.Color('gold')), (535, 735))
-
+        # конец игры
         for i in range(Width):
             if field[0][i]:
                 set_record(record, score)
-                field = [[0 for i in range(Width)] for i in range(Height)]
+                field = [[0 for _ in range(Width)] for _ in range(Height)]
+                # обнуление значений
                 anim_count = 0
                 anim_speed = 60
                 anim_limit = 2000
                 score = 0
+                # конечная заставка
                 for irect in grid:
                     pygame.draw.rect(game_sc, get_color(), irect)
                     sc.blit(game_sc, (20, 20))
@@ -191,7 +214,10 @@ while True:
         pygame.display.flip()
         clock.tick(FPS)
     else:
-        title_pause = pygame.font.Font('font/Vasek.ttf', 230).render('Пауза', True,pygame.Color(randrange(30, 256), randrange(30, 256), randrange(30, 256), randrange(30, 256)))
+        title_pause = pygame.font.Font('font/Vasek.ttf', 230).render('Пауза', True, pygame.Color(randrange(30, 256),
+                                                                                                 randrange(30, 256),
+                                                                                                 randrange(30, 256),
+                                                                                                 randrange(30, 256)))
         sc.blit(pause_bg, (0, 0))
         sc.blit(title_pause, (200, 400))
         pygame.display.flip()
